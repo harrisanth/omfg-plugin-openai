97203d95d2b2f073d25a45b6688d97904c5c6968
crosshj <github@chimpjuice.com>
2023-04-22 20:30:37 -0400

    Create plugin.js

---
+++ plugin.js
@@ -0,0 +1,21 @@
+export const agentSettings = {
+  model: {
+    type: "input",
+    label: "Model",
+  },
+  initial_prompt: {
+    type: "text_area",
+    label: "Initial Prompt",
+    description: "Enter intial instructions for the agent."
+  },
+  initial_message: {
+    type: "text_area",
+    label: "Initial Message",
+    description: "Extra instructions included with every message."
+  },
+};
+
+export const agentMessage = async (...args) => {
+  console.log({ args });
+  return 'I am a bot based on omfg-plugin-openai.  I currently do nothing interesting.';
+};
